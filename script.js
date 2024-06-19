38ee7fc7588ace6e4961bc85ab825bc7b7fb1b16
Tarun Rawat <144971861+tarun05rawat@users.noreply.github.com>
2024-06-19 19:04:18 +0400

    final commit

--- script.js
+++ script.js
@@ -5,10 +5,43 @@ function addTask() {
   if (inputBox.value === ''){
     alert('Please enter a task');
   }
+  
   else {
     let li = document.createElement("li");
-    li.innerHTML = inputBox.value();
+    li.innerHTML = inputBox.value;
     listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = '\u00D7';
+    li.appendChild(span);
   }
-
+  inputBox.value = '';
+  saveData();
 }
+
+function handleKeyPress(event) {
+  if (event.keyCode === 13) {
+    addTask();
+  }
+}
+
+inputBox.addEventListener('keypress', handleKeyPress);
+listContainer.addEventListener('click', function(e) {
+  if (e.target.tagName === 'LI') {
+    e.target.classList.toggle('checked');
+    saveData();
+  }
+  else if (e.target.tagName === 'SPAN') {
+    e.target.parentElement.remove();
+    saveData();
+  }
+}, false);
+
+function saveData() {
+  localStorage.setItem('data', listContainer.innerHTML);
+}
+
+function loadData() {
+  listContainer.innerHTML = localStorage.getItem('data');
+}
+loadData();
+
